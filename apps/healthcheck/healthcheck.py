c73da19f6dac94db67d46641e596d8fc45a6fbb1
Ben Lebherz <git@benleb.de>
2020-11-01 17:19:41 +0100

    remove external deps

--- apps/healthcheck/healthcheck.py
+++ apps/healthcheck/healthcheck.py
@@ -8,32 +8,41 @@ healthcheck:
   endpoint: healthcheck
 """
 
-from typing import Any, Dict, Tuple
+__version__ = "0.4.4"
 
-from adutils import ADutils
+from typing import Any, Dict, Optional, Tuple, Union
+
 import hassapi as hass
 
 APP_NAME = "Healthcheck"
 APP_ICON = "🏥"
-APP_VERSION = "0.4.4"
+
+
+def hl(text: Union[int, float, str]) -> str:
+    return f"\033[1m{text}\033[0m"
 
 
 class Healthcheck(hass.Hass):  # type: ignore
     """Healthcheck."""
 
+    def lg(self, msg: str, *args: Any, icon: Optional[str] = None, repeat: int = 1, **kwargs: Any) -> None:
+        kwargs.setdefault("ascii_encode", False)
+        message = f"{f'{icon} ' if icon else ' '}{msg}"
+        _ = [self.log(message, *args, **kwargs) for _ in range(repeat)]
+
     async def initialize(self) -> None:
         """Register API endpoint."""
-        self.cfg: Dict[str, Any] = dict()
-        self.cfg["endpoint"] = str(self.args.get("endpoint", APP_NAME))
+        endpoint = str(self.args.get("endpoint", APP_NAME))
 
         # register api endpoint
-        await self.register_endpoint(self.healthcheck, self.cfg["endpoint"])
-
-        # set prefix
-        self.cfg.setdefault("_prefixes", dict(endpoint="/"))
+        await self.register_endpoint(self.healthcheck, endpoint)
 
         # show active config / init adutils
-        ADutils(APP_NAME, self.cfg, icon=APP_ICON, ad=self, show_config=True)
+        self.lg("")
+        self.lg(f"{hl(APP_NAME)} v{hl(__version__)}", icon=APP_ICON)
+        self.lg("")
+        self.lg(f"    endpoint: /{hl(endpoint)}")
+        self.lg("")
 
     async def healthcheck(self, _: Any) -> Tuple[Dict[str, Any], int]:
         """Handle incoming requests."""
